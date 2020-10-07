463dec17e6e83ab5b0daa06fa3071441a9c81caf
PlaksinaKate <plaksina.larisa@yandex.ru>
2020-10-07 09:32:03 +0300

    Initial

--- src/com/company/DrawPanel.java
+++ src/com/company/DrawPanel.java
@@ -4,10 +4,10 @@ import javax.swing.*;
 import java.awt.*;
 
 public class DrawPanel extends JPanel {
-    private Sky sky = new Sky(0, 0, 1500, 500, new Color(233, 234, 255), new Color(120, 124, 232));
+    private Sky sky = new Sky(0, 0, 1500, 500, new Color(233, 234, 255), new Color(120, 124, 232),Color.black);
     private Ground ground = new Ground(0, 500, 1500, 500, new Color(98, 252, 108), new Color(64, 168, 48));
     private Home home = new Home(400, 300, 1300, 750, new Color(84, 61, 61), new Color(41, 29, 29), new Color(39, 40, 35), new Color(252, 255, 199), new Color(255, 255, 153));
-    private ObjectsNearTheHouse objectsNearTheHouse = new ObjectsNearTheHouse(400, 300, 1300, 750, new Color(119, 136, 153), new Color(112, 128, 144), new Color(135, 206, 235), new Color(85, 220, 36),new Color(26,192,64));
+    private ObjectsNearTheHouse objectsNearTheHouse = new ObjectsNearTheHouse(400, 300, 1300, 750, new Color(119, 136, 153), new Color(112, 128, 144), new Color(135, 206, 235), new Color(85, 220, 36), new Color(26, 192, 64));
 
     @Override
     public void paint(Graphics g) {
--- src/com/company/Sky.java
+++ src/com/company/Sky.java
@@ -4,13 +4,14 @@ import java.awt.*;
 
 public class Sky implements Drawable {
     private int x, y, width, height;
-    private Color c1, c2;
+    private Color c1, c2, bird;
     private Sun s = new Sun(5, 5, 120, 120, new Color(247, 252, 98));
 
     @Override
     public void draw(Graphics2D gr) {
         sky(gr, x, y, width, height, c1, c2);
         s.draw(gr);
+        bird(gr, width, height, bird);
     }
 
     public static void sky(Graphics2D gr, int x, int y, int width, int height, Color c1, Color c2) {
@@ -20,12 +21,24 @@ public class Sky implements Drawable {
         gr.fillRect(x, y, width, height);
     }
 
-    public Sky(int x, int y, int width, int height, Color c1, Color c2) {
+    public static void bird(Graphics2D gr, int width, int height, Color bird) {
+        gr.setColor(bird);
+        int num = 0;
+        while (num < 20) {
+            int i = (int) (Math.random() * width);
+            int j = (int) (Math.random() * height);
+            gr.fillPolygon(new int[]{i, i + 15, i + 30, i + 15}, new int[]{j + 15, j + 30, j + 15, j + 25}, 4);
+            num++;
+        }
+    }
+
+    public Sky(int x, int y, int width, int height, Color c1, Color c2, Color bird) {
         this.x = x;
         this.y = y;
         this.width = width;
         this.height = height;
         this.c1 = c1;
         this.c2 = c2;
+        this.bird = bird;
     }
 }
